ad066192d495b7be8f61c05405e9bb17bac57020
ebierman <elibierman@gmail.com>
2014-03-31 01:55:52 -0400

    added basic automated test for user creation

---
+++ test_users.py
@@ -0,0 +1,29 @@
+import datetime
+
+from django.test import TestCase
+from users.models import User
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(id=1,
+            username='eli',
+            email='elibierman@gmail.com',
+            password='please',
+            website='elibierman.com',
+            city='Ridgewood',
+            joindate=datetime.datetime.now(),
+            )
+        User.objects.create(id=2,
+            username='amardeep',
+            email='amardeep34@gmail.com',
+            password='please',
+            website='portfolio.amardeeps.com',
+            city='Ridgewood',
+            joindate=datetime.datetime.now(),
+            )
+
+    def test_post_have_category(self):
+        first_post = Post.objects.get(id=1)
+        second_post = Post.objects.get(id=2)
+        self.assertEqual(first_post.email, 'elibierman@gmail.com')
+        self.assertEqual(second_post.email, 'amardeep34@gmail.com')
