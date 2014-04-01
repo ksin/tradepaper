8517616e1785927e4677e1aff5f6b22a4e6c172a
ebierman <elibierman@gmail.com>
2014-03-31 23:48:25 -0400

    removed more old refs to paperapp

--- tradepaper/urls.py
+++ tradepaper/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^users/', include('paperapp.urls')),
+    url(r'^users/', include('users.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from paperapp import views
+from users import views
 
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
