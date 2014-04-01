d226684d2d41d38de72b25b5733816b51aad9a66
ebierman <elibierman@gmail.com>
2014-03-31 21:45:45 -0400

    modified fabfile to not handle commits for deployment preparation

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,14 @@
 from fabric.api import lcd, local
 
 def prepare_deployment(branch_name):
-  local('./manage.py test tradepaper')
-  local('git add -A . && git commit')
+    local('./manage.py test tradepaper')
+    # local('git add -A . && git commit')
 
 def deploy():
-  with lcd('~/Documents/Apps/Django/tradepaper/'):
-    local('git pull ~/Documents/Apps/Django/dev/tradepaper/')
+    with lcd('~/Documents/Apps/Django/tradepaper/'):
+        local('git pull ~/Documents/Apps/Django/dev/tradepaper/')
 
-    #users app
-    local('./manage.py migrate users')
-    local('./manage.py test users')
-    local('./manage.py runserver 0.0.0.0:8000') #change this to prod server when you have one
+        #users app
+        local('./manage.py migrate users')
+        local('./manage.py test users')
+        local('./manage.py runserver 0.0.0.0:8000') #change this to prod server when you have one
