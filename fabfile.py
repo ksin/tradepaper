afdd1632d5f96bc89944cb4d2a13331748071c83
ebierman <elibierman@gmail.com>
2014-03-31 22:11:16 -0400

    messing with fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,13 @@
 from fabric.api import lcd, local
 
-def prepare_deployment(branch_name):
+def prep_deploy(branch_name):
     local('./manage.py test tradepaper')
-    # local('git add -A . && git commit')
+    local('git add -A . && git commit')
 
 def deploy():
     with lcd('~/Documents/Apps/Django/tradepaper/'):
         local('git pull ~/Documents/Apps/Django/dev/tradepaper/')
+        local('git push origin master')
 
         #users app
         local('./manage.py migrate users')
