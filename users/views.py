395dae2953fe6202cde7e1b5e618e93dec780666
mumio <elibierman64@startmail.com>
2014-04-02 16:04:00 -0400

    made all fields on register page part of the same form

--- users/views.py
+++ users/views.py
@@ -24,7 +24,7 @@ def register(request):
         form = UserForm(request.POST, auto_id='signup_%s')
         if form.is_valid():
             new_user = form.save()
-            return render(request, )
+            return render(request, paperapp/users/)
     else:
         form = UserForm() # An unbound form
 
