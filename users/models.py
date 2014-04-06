7e51b7c161a41e6fc9090881735db73024db4718
mumio <elibierman64@startmail.com>
2014-04-06 15:41:13 -0400

    ensuring uniqueness of username and email fields

--- users/models.py
+++ users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique=True)
     password = models.CharField(max_length=30)
-    email = models.EmailField()
+    email = models.EmailField(unique=True, )
     city = models.CharField(max_length=60, blank=True)
     website = models.URLField(blank=True)
     joindate = models.DateTimeField()
