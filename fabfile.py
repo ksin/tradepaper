79902b5988ed6cbcdc3c459f9a38524669a88cf5
mumio <elibierman64@startmail.com>
2014-05-08 22:26:03 -0400

    reconfigured settings to allow for development and production settings

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,23 @@
 from fabric.api import lcd, local
 
+# env.user = 'eli'
+# env.hosts = ['198.58.125.106']
+
 def prep_deploy(branch_name):
     local('./manage.py test tradepaper')
     local('git add -A . && git commit')
     local('git push origin %s' % branch_name)
 
-def deploy():
+def deploy_dev():
+    with lcd('~/Documents/Apps/Django/tradepaper/'):
+        local('git pull origin master')
+
+        #users app
+        local('./manage.py migrate users')
+        local('./manage.py test users')
+        local('./manage.py runserver localhost:8000')
+
+def deploy_prod():
     with lcd('~/Documents/Apps/Django/tradepaper/'):
         local('git pull origin master')
 
