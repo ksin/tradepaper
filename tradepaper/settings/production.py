5a3d13cea49ccaf981964c9b2b591e997aa9008a
mumio <elibierman64@startmail.com>
2014-05-09 09:51:11 -0400

    added postgres database settings for production

--- tradepaper/settings/production.py
+++ tradepaper/settings/production.py
@@ -1,3 +1,14 @@
 from common import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tradepaper',
+        'USER': 'eli',
+        'PASSWORD': 'maggie',
+        'HOST': 'localhost',
+        'PORT': '',                # Set to empty string for default.
+    }
+}
