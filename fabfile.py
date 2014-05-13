ab2a08055d9e3fe159abdebefe563a54e9727111
mumio <elibierman64@startmail.com>
2014-05-13 09:51:46 -0400

    more fab

--- fabfile.py
+++ fabfile.py
@@ -27,6 +27,7 @@ def deploy_prod():
         run('echo $DJANGO_SETTINGS_MODULE')
     with cd(app_dir), prefix('source bin/activate'):
         run('python --version')
+        run('echo $SHELL')
         run('which python')
         run('which pip')
         run('pip install -r requirements.txt --allow-all-external')
