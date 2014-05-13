f051dacdad4b3bdb33182b8abd9f6b2ae73c9e36
mumio <elibierman64@startmail.com>
2014-05-13 09:45:19 -0400

    more fab tweaks

--- fabfile.py
+++ fabfile.py
@@ -26,6 +26,7 @@ def deploy_prod():
         run('export DJANGO_SETTINGS_MODULE=tradepaper.settings.production')
         run('echo $DJANGO_SETTINGS_MODULE')
     with cd(app_dir), prefix('source bin/activate'):
+        run('python --version')
         run('which python')
         run('which pip')
         run('pip install -r requirements.txt --allow-all-external')
