c2b71e61c4f989254eebd829dc7b8962c434b612
mumio <elibierman64@startmail.com>
2014-05-12 22:52:49 -0400

    modified production script, but it's untested; still need to add SSH key for github

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,11 @@
-from fabric.api import lcd, local
-
-# env.user = 'eli'
-# env.hosts = ['198.58.125.106']
+from fabric.api import lcd, local, hosts, run, cd
 
 def prep_deploy(branch_name):
     local('./manage.py test tradepaper')
     local('git add -A . && git commit')
     local('git push origin %s' % branch_name)
 
-def deploy_staging():
+def deploy_dev():
     with lcd('~/Documents/Apps/Django/tradepaper/'):
         local('git pull origin master')
         local('export DJANGO_SETTINGS_MODULE=tradepaper.settings')
@@ -20,11 +17,19 @@ def deploy_staging():
         local('./manage.py test users')
         local('./manage.py runserver localhost:8000')
 
+@hosts('eli@beta.trade-paper.com')
 def deploy_prod():
-    with lcd('~/Documents/Apps/Django/tradepaper/'):
-        local('git pull origin master')
+    app_dir = '/webapps/paper-py2/'
+    git_dir = '/webapps/paper-py2/tradepaper'
+    with cd(app_dir):
+        run('source bin/activate')
+        run('pip install -r requirements.txt --allow-all-external')
+    with cd(git_dir), run('source ../bin/activate'):
+        run('git pull origin master')
+        run('export DJANGO_SETTINGS_MODULE=tradepaper.settings.production')
+        run('echo $DJANGO_SETTINGS_MODULE')
 
         #users app
-        local('./manage.py migrate users')
-        local('./manage.py test users')
-        local('./manage.py runserver localhost:8000') #change this to prod server when you have one
+        run('./manage.py migrate users')
+        run('./manage.py test users')
+        run('./manage.py runserver localhost:8000')
