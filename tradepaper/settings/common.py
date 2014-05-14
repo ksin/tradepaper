90a936f331db17a63f1d8237d309b62de4fa0dfc
mumio <elibierman64@startmail.com>
2014-05-14 09:31:28 -0400

    moved static files and ran collectstatic

--- tradepaper/settings/common.py
+++ tradepaper/settings/common.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -94,6 +95,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 
 LOGGING = {
     'version': 1,
