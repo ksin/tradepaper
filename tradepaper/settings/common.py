844e8532f097af6e022aa495518b3ac92d109d72
mumio <elibierman64@startmail.com>
2014-05-16 09:24:37 -0400

    changed path to debug.log to be relative

--- tradepaper/settings/common.py
+++ tradepaper/settings/common.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+PROJECT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -105,7 +105,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/Users/Ohio/Documents/Apps/Django/dev/tradepaper/debug.log',
+            'filename': os.path.join(PROJECT_DIR, 'debug.log'),
         },
     },
     'loggers': {
