a57b1c78bbd2a83c1685e901c72011ca475b0bbd
mumio <elibierman64@startmail.com>
2014-05-16 20:07:58 -0400

    changed fabfile so it runs successfully on production environment

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import lcd, local, hosts, run, cd, prefix
+from fabric.api import lcd, local, hosts, run, cd, prefix, sudo
 
 def prep_deploy(branch_name):
     local('./manage.py test tradepaper')
@@ -13,6 +13,7 @@ def deploy_dev():
         local('pip install -r requirements.txt --allow-all-external')
 
         #users app
+        local('./manage.py syncdb')
         local('./manage.py migrate users')
         local('./manage.py test users')
         local('./manage.py runserver localhost:8000')
@@ -21,15 +22,15 @@ def deploy_dev():
 def deploy_prod():
     app_dir = '/webapps/paper-py2/'
     git_dir = '/webapps/paper-py2/tradepaper'
-    with prefix('source %s/bin/activate' % app_dir):
-        with cd(git_dir):
-            run('git pull origin master')
-            run('export DJANGO_SETTINGS_MODULE=tradepaper.settings.production')
-            run('echo $DJANGO_SETTINGS_MODULE')
-        with cd(app_dir), prefix('source bin/activate'):
+    export_prod_settings = ' && export DJANGO_SETTINGS_MODULE=tradepaper.settings.production'
+    with cd(git_dir):
+        run('git pull origin master')
+    with prefix(('source %s/bin/activate' % app_dir) + export_prod_settings):
+        run('echo $DJANGO_SETTINGS_MODULE')
+        with cd(app_dir):
             run('pip install -r requirements.txt --allow-all-external')
-        with cd(git_dir), prefix('source ../bin/activate'):
+        with cd(git_dir):
             #users app
+            run('./manage.py syncdb')
             run('./manage.py migrate users')
-            run('./manage.py test users')
-            run('./manage.py runserver localhost:8000')
+            sudo('supervisorctl restart tradepaper')
