5fbc34394bf607adf1876f821992f7a9463f44cf
mumio <elibierman64@startmail.com>
2014-06-19 23:02:06 -0400

    now actually testing whether a user logs in successfully

--- users/tests.py
+++ users/tests.py
@@ -8,20 +8,22 @@ from users.models import User
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(id=1,
+        u1 = User.objects.create(id=1,
             username='eli',
             email='elibierman@gmail.com',
             password='please',
             website='elibierman.com',
             city='Ridgewood',
             )
-        User.objects.create(id=2,
+        u1.save()
+        u2 = User.objects.create(id=2,
             username='amardeep',
             email='amardeep34@gmail.com',
             password='please',
             website='portfolio.amardeeps.com',
             city='Ridgewood',
             )
+        u2.save()
 
     def test_users_have_category(self):
         first_user = User.objects.get(id=1)
@@ -39,22 +41,30 @@ def create_user(username, email, password):
 class UserViewTest(TestCase):
     def test_create_and_log_in_user_with_correct_credentials(self):
         user = create_user('eli', 'eli@me.com', 'ok')
-        c = Client()
-        logged_in = c.login(username='eli', password='ok')
-        self.assertEqual(logged_in, True)
+        response = self.client.post(reverse('users:login'), {'username':'eli', 'password':'ok'}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('_auth_user_id', self.client.session)
+        self.assertEqual(self.client.session['_auth_user_id'], user.pk)
 
-    def test_create_and_log_in_user_with_wrong_password(self):
-        user = create_user('eli', 'eli@me.com', 'ok')
-        c = Client()
-        logged_in = c.login(username='eli', password='NOk')
-        self.assertEqual(logged_in, False)
-
-    def test_create_and_log_in_user_with_wrong_username(self):
-        user = create_user('eli', 'eli@me.com', 'ok')
-        c = Client()
-        logged_in = c.login(username='fred', password='ok')
-        self.assertEqual(logged_in, False)
-
+    # def test_create_and_log_in_user_with_wrong_password(self):
+    #     user = create_user('eli', 'eli@me.com', 'ok')
+    #     login_response = self.client.get(reverse('users:login'))
+    #     self.assertEqual(login_response.status_code, 200)
+    #     logged_in_response = self.client.get(reverse('users:login'))
+    #     self.assertEqual(response.request.user.is_authenticated(), True)
+    #
+    # def test_create_and_log_in_user_with_wrong_username(self):
+    #     user = create_user('eli', 'eli@me.com', 'ok')
+    #     c = Client()
+    #     logged_in = c.login(username='eli', password='ok')
+    #     self.assertEqual(logged_in, True)
+    #     logged_out = c.logout()
+    #     self.assertEqual(logged_in, True)
+    #
     # def test_create_and_log_in_user_and_log_out(self):
+    #     user = create_user('eli', 'eli@me.com', 'ok')
+    #     c = Client()
+    #     logged_in = c.login(username='fred', password='ok')
+    #     self.assertEqual(logged_in, False)
     #
     # def test_create_and_log_in_user_and_check_user_index(self):
