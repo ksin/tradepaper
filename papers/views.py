c8c7e3e1a65d58ad63ebbceb29c2d9d4d258ad44
mumio <elibierman64@startmail.com>
2014-08-13 16:40:27 -0400

    added URL scheme for single listing page now displaying generic listing

--- papers/views.py
+++ papers/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from papers.models import Listing
+from users.models import User
+
 def browse(request):
     return render(request, 'tradepaper/browse.html')
+
+def listing(request, name, title, edition):
+    u = get_object_or_404(User, name=name)
+    listing = get_object_or_404(Listing, user=u, title=title, edition=edition)
+    return render(request, 'tradepaper/single-paper.html', {'listing': listing})
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
 from users import views
+from papers import views as papers_views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<name>\w+)$', views.profile, name='profile'),
+    url(r'^(?P<name>\w+)/(?P<title>\w+)/(?P<edition>\w+)$', papers_views.listing, name='listing'),
 )
