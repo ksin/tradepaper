d5aff730fb76d0caa2d35fd77d8ed74901d2a579
mumio <elibierman64@startmail.com>
2014-09-29 20:04:10 -0500

    changed papers migrations

--- papers/migrations/0001_initial.py
+++ papers/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(SchemaMigration):
             ('edition', self.gf('django.db.models.fields.CharField')(max_length=60)),
             ('condition', self.gf('django.db.models.fields.IntegerField')()),
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['users.User'])),
-            ('date_posted', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 8, 15, 0, 0))),
-            ('image_url', self.gf('django.db.models.fields.URLField')(max_length=200)),
+            ('date_posted', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 9, 29, 0, 0))),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
         db.send_create_signal(u'papers', ['Listing'])
 
@@ -30,17 +30,17 @@ class Migration(SchemaMigration):
         u'papers.listing': {
             'Meta': {'object_name': 'Listing'},
             'condition': ('django.db.models.fields.IntegerField', [], {}),
-            'date_posted': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 8, 15, 0, 0)'}),
+            'date_posted': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 9, 29, 0, 0)'}),
             'edition': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['users.User']"})
         },
         u'users.user': {
             'Meta': {'object_name': 'User'},
             'city': ('django.db.models.fields.CharField', [], {'max_length': '60', 'blank': 'True'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 8, 15, 0, 0)'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 9, 29, 0, 0)'}),
             'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
