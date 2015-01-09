b9f6032e55fbae1c9b3b2b041febe513d0c4c5fb
ebierman <elib@startmail.com>
2015-01-09 17:42:58 -0500

    added gunicorn config file

---
+++ conf/gunicorn.py
@@ -0,0 +1,5 @@
+pidfile = '/webapps/trade-paper.com/tmp/gunicorn.pid'
+bind = '198.58.125.106'
+workers = 3
+accesslog = '/webapps/trade-paper.com/logs/gunicorn-access.log'
+errorlog = '/webapps/trade-paper.com/logs/gunicorn-error.log'
