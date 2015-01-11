85fdc9515468f3bd5b036c95ca0e5e0086f58427
ebierman <elib@startmail.com>
2015-01-10 17:38:14 -0700

    moved database setting into dev from common

--- tradepaper/settings/common.py
+++ tradepaper/settings/common.py
@@ -66,16 +66,6 @@ ROOT_URLCONF = 'tradepaper.urls'
 WSGI_APPLICATION = 'tradepaper.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(TOP_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
