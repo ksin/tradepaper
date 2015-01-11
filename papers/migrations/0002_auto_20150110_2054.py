70df5b703c5e4cd3465e58440111107bc4f563da
ebierman <elib@startmail.com>
2015-01-10 18:55:22 -0700

    added migrations

---
+++ papers/migrations/0002_auto_20150110_2054.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('papers-app', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='listing',
+            name='date_posted',
+            field=models.DateTimeField(default=datetime.datetime(2015, 1, 11, 1, 54, 48, 632332, tzinfo=utc)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 1, 11, 1, 54, 48, 634115, tzinfo=utc)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='request',
+            name='date_initiated',
+            field=models.DateTimeField(default=datetime.datetime(2015, 1, 11, 1, 54, 48, 633210, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
---
+++ users/migrations/0002_auto_20150110_2054.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='date_joined',
+            field=models.DateTimeField(default=datetime.datetime(2015, 1, 11, 1, 54, 48, 630693, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
