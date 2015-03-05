13c85095d6b152462489fe2aeaaa322043814f68
ebierman <elib@startmail.com>
2015-03-04 23:33:09 -0500

    added migrations

---
+++ papers/migrations/0006_auto_20150304_2333.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('papers-app', '0005_auto_20150112_0158'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='listing',
+            name='date_posted',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 5, 4, 33, 7, 4764, tzinfo=utc)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='message',
+            name='date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 5, 4, 33, 7, 6205, tzinfo=utc)),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='request',
+            name='date_initiated',
+            field=models.DateTimeField(default=datetime.datetime(2015, 3, 5, 4, 33, 7, 5567, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
