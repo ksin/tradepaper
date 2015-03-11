0657825774288b8dad27798ff03b659c6a2efe8a
ebierman <elib@startmail.com>
2015-03-11 16:41:20 -0400

    hooked up faq template

--- general/urls.py
+++ general/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 
 from general import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
-    url(r'^about/$', views.about, name='about'),
-)
+# urlpatterns = patterns('',
+#     url(r'^$', views.home, name='home'),
+#     url(r'^about/$', views.about, name='about'),
+#     url(r'^faq/$', views.faq, name='faq'),
+# )
--- general/views.py
+++ general/views.py
@@ -7,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request, 'tradepaper/about.html')
+
+def faq(request):
+    return render(request, 'tradepaper/faq.html')
