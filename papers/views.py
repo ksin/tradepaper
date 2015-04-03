d2ac439711d9fd98599552c744f2e955f2423849
ebierman <elib@startmail.com>
2015-04-03 15:18:27 -0400

    began implementing trade request offering

--- papers/views.py
+++ papers/views.py
@@ -6,6 +6,7 @@ from django import forms
 
 from papers.models import Listing, Request
 from users.models import User
+from users.views import vet_user
 
 class ListingForm(forms.ModelForm):
     class Meta:
@@ -22,6 +23,7 @@ def listing(request, id):
     l = get_object_or_404(Listing, id=id)
     return render(request, 'tradepaper/single-paper.html', {'listing':l})
 
+@vet_user("You need to be logged in to create a listing.")
 def new_listing(request):
     user = request.user
     form = ListingForm(request.POST, request.FILES, auto_id='new_listing_%s')
@@ -40,28 +42,24 @@ def new_listing(request):
             messages.error(request, 'Please fill in all required fields')
             return render(request, 'tradepaper/new-listing.html', {'form': form})
     else:
-        if user.is_authenticated():
-            return render(request, 'tradepaper/new-listing.html')
-        else:
-            messages.error(request, "You need to be logged in to create a listing.")
-            return HttpResponseRedirect(reverse('login'))
+        return render(request, 'tradepaper/new-listing.html')
 
 def request(request, id):
     r = get_object_or_404(Request, id=id)
     return render(request, 'tradepaper/trade-request.html', {'request': r})
 
+@vet_user("You need to be logged in to make a trade request.")
 def new_request(request, listing_id):
     listing = get_object_or_404(Listing, id=listing_id)
+    user = request.user
+    # create request, but don't save it until it's posted
+    r = Request(
+            requester = user,
+            requestee = listing.user,
+            listing = listing
+            )
     if request.method == 'POST':
-        user = request.user
-        if(user.is_authenticated()):
-            r = Request(
-                    requester = user,
-                    requestee = listing.user,
-                    listing = listing
-                    )
-            r.save()
-            return HttpResponseRedirect(reverse('papers:request', args=(r.id,)))
-        else:
-            messages.error(request, "You need to be logged in to make a trade request.")
-            return HttpResponseRedirect(reverse('login'))
+        r.save()
+        return HttpResponseRedirect(reverse('papers:request', args=(r.id,)))
+    else:
+        return render(request, 'tradepaper/trade-request.html', {'request': r})
