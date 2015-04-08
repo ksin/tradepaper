89d5a785cb077a4db25b965e4ab80d8b922fc1fc
ebierman <elib@startmail.com>
2015-04-08 17:41:14 -0400

    added migrations

---
+++ users/migrations/0013_auto_20150408_1641.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0012_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='date_joined',
+            field=models.DateTimeField(default=datetime.datetime(2015, 4, 8, 21, 41, 10, 822566, tzinfo=utc)),
+            preserve_default=True,
+        ),
+    ]
