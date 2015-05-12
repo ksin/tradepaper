8aa803185fc361c5cd6cf5a0eb9ba0fb0b088148
ebierman <elib@startmail.com>
2015-05-12 13:51:00 -0400

    fixed datetime error on model fields

--- papers/models.py
+++ papers/models.py
@@ -1,6 +1,5 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.utils import timezone
 from shortuuidfield import ShortUUIDField
 
 class Listing(models.Model):
@@ -9,7 +8,7 @@ class Listing(models.Model):
     edition = models.CharField(max_length=60)
     condition = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(10)])
     user = models.ForeignKey('users.User')
-    date_posted = models.DateTimeField(default=timezone.now())
+    date_posted = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images')
 
     def __str__(self):
@@ -17,7 +16,7 @@ class Listing(models.Model):
 
 class Trade(models.Model):
     id = ShortUUIDField(primary_key=True, unique=True)
-    date_initiated = models.DateTimeField(default=timezone.now())
+    date_initiated = models.DateTimeField(auto_now_add=True)
     listing = models.ForeignKey(Listing)
     trader = models.ForeignKey('users.User', related_name='trades_sent')
     tradee = models.ForeignKey('users.User', related_name='trades_received')
@@ -28,7 +27,7 @@ class Trade(models.Model):
 class Message(models.Model):
     id = ShortUUIDField(primary_key=True, unique=True)
     sent_by_trader = models.BooleanField(default=True)
-    date = models.DateTimeField(default=timezone.now())
+    date = models.DateTimeField(auto_now_add=True)
     text = models.TextField(max_length=4096)
     trade = models.ForeignKey(Trade, related_name='messages', null=True)
     read = models.BooleanField(default=False)
--- users/models.py
+++ users/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.utils import timezone
 from papers.models import Message
 from shortuuidfield import ShortUUIDField
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password, name, city, is_staff=False, is_superuser=False):
-        now = timezone.now()
         email = self.normalize_email(email)
         user = self.model(email=email, name=name,
                           city=city, is_active=True,
-                          is_staff=is_staff, is_superuser=is_superuser,
-                          last_login=now, date_joined=now)
+                          is_staff=is_staff, is_superuser=is_superuser)
         user.set_password(password)
         user.save()
         return user
@@ -26,7 +23,7 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=40, unique=True)
     city = models.CharField(max_length=60, blank=True)
     website = models.URLField(blank=True)
-    date_joined = models.DateTimeField(default=timezone.now())
+    date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
